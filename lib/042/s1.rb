b763e252b2662942affb22181687dd25028d17c5
sahglie <sahglie@gmail.com>
2011-01-30 08:46:47 -0800

    1st solution to #42

---
+++ lib/042/s1.rb
@@ -0,0 +1,22 @@
+
+def score(word)
+  word.each_char.inject(0) { |score, c| score += (c.ord - 96) }
+end
+
+def tri_num?(n)
+  tri_nums_upto(n).include?(n)
+end
+
+def tri_nums_upto(num)
+  (1..num).inject([]) do |tri_ary, n|
+    tri = (n*(n+1))/2
+    return tri_ary if tri > num
+    tri_ary << tri
+  end
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  words = File.readlines("data.txt").first.split(",").map { |wrd| wrd[/\w+/].downcase }
+  puts words.select { |wrd| tri_num?(score(wrd)) }.count
+end
