f40a95370cef1ee87fac0ae5563c09ff2849b9a4
sahglie <sahglie@gmail.com>
2011-01-29 23:11:38 -0800

    1st solution to #36

---
+++ lib/036/s1.rb
@@ -0,0 +1,13 @@
+
+def b2_b10_palindromes(upperbound)
+  (1...upperbound).select do |n|
+    b10 = n.to_s.split("")
+    b2 = n.to_s(2).split("")
+    (b10 == b10.reverse) && (b2 == b2.reverse)
+  end
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  puts b2_b10_palindromes(1_000_000).inject(:+)
+end
